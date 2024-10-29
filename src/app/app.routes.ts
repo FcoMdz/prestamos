b92699ea53d4a6ba97f99829accdfab6bb8c97c1
Francisco Mendez <fcodmendez24@gmail.com>
2024-10-28 21:18:49 -0600

    Generación de componentes e instalación bootstrap

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { LandingComponent } from './compontents/landing/landing.component';
+import { CotizacionComponent } from './compontents/cotizacion/cotizacion.component';
+import { LoginComponent } from './compontents/login/login.component';
+import { SingupComponent } from './compontents/singup/singup.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    { path: '', redirectTo: '/home', pathMatch: 'full' }, // Redirige a 'home' por defecto
+    { path: 'home', component: LandingComponent }, // Ruta para el componente de la página principal
+    { path: 'cotizar', component: CotizacionComponent }, // Ruta para el componente para realiar cotizaciones
+    { path: 'cotizar', component: CotizacionComponent }, // Ruta para el componente para realiar cotizaciones
+    { path: 'login', component: LoginComponent }, // Ruta para el componente para realiar cotizaciones
+    { path: 'signup', component: SingupComponent }, // Ruta para el componente para realiar cotizaciones
+    { path: '**', component: LandingComponent }, // Manejo de rutas no encontradas (404)
+  
+];
---
+++ src/app/compontents/cotizacion/cotizacion.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cotizacion',
+  standalone: true,
+  imports: [],
+  templateUrl: './cotizacion.component.html',
+  styleUrl: './cotizacion.component.scss'
+})
+export class CotizacionComponent {
+
+}
