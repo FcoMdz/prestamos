4625a21a06306aaf421309db8bf62dd1a21d28a1
Francisco Mendez <fcodmendez24@gmail.com>
2024-10-31 00:18:32 -0600

    signup e historial

--- Front/src/app/compontents/general/navbar/navbar.component.ts
+++ Front/src/app/compontents/general/navbar/navbar.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterModule, RouterOutlet } from '@angular/router';
+import { Router } from 'express';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet,RouterLink, RouterModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+  constructor(){
 
+  }
+  gotoLogin(){
+    console.log("holi")
+  }
 }
--- Front/src/app/compontents/singup/singup.component.ts
+++ Front/src/app/compontents/singup/singup.component.ts
@@ -1,12 +1,60 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-singup',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './singup.component.html',
   styleUrl: './singup.component.scss'
 })
 export class SingupComponent {
+  registerForm: FormGroup;
 
+  municipios = ['Municipio 1', 'Municipio 2', 'Municipio 3'];
+  estados = ['Estado 1', 'Estado 2', 'Estado 3'];
+  paises = ['País 1', 'País 2', 'País 3'];
+
+  constructor(private fb: FormBuilder) {
+    this.registerForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      nombre: ['', [Validators.required, Validators.minLength(2)]],
+      apellidoPaterno: ['', [Validators.required, Validators.minLength(2)]],
+      apellidoMaterno: ['', Validators.minLength(2)],
+      telefono: [
+        '',
+        [Validators.required, Validators.pattern('^[0-9]{10}$')] // 10 dígitos
+      ],
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.pattern('^(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*]).+$')
+        ]
+      ],
+      direccion: this.fb.group({
+        calle: ['', Validators.required],
+        codigoPostal: [
+          '',
+          [Validators.required, Validators.pattern('^[0-9]{5}$')]
+        ],
+        noInt: [''],
+        noExt: ['', Validators.required],
+        colonia: ['', Validators.required],
+        municipio: ['', Validators.required],
+        estado: ['', Validators.required],
+        pais: ['', Validators.required]
+      })
+    });
+  }
+
+  onSubmit() {
+    if (this.registerForm.valid) {
+      console.log('Formulario enviado:', this.registerForm.value);
+    } else {
+      console.log('Formulario inválido');
+    }
+  }
 }
