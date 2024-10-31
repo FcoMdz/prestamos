b404c9db10faa6e187bdfc60e5f66865fd48b416
gerardo1209 <git@femat.dev>
2024-10-31 00:42:51 -0600

    all apis

--- Backend/index.js
+++ Backend/index.js
@@ -17,7 +17,6 @@ const genContrasena = async (contrasena) => {
     const saltRounds = 15;
     try{
         const hashedContasena = await bcrypt.hash(contrasena,saltRounds);
-        console.log(hashedContasena)
         return hashedContasena;
     }catch(err){
         console.log(err);
@@ -28,7 +27,19 @@ const revContrasena = async (contrasena, usuario) => {
     q = `SELECT contrasena FROM Empleado WHERE usuario = ?;`;
     const data = await pool.query(q,[usuario]);
     var band = false;
-    var hashedContasena = data[0].contrasena;
+    var hashedContasena = data[0][0].contrasena;
+    await bcrypt.compare(contrasena, hashedContasena).then((result) => {
+        band=result;
+    });
+    return band;
+}
+
+
+const revContrasenaUsuario = async (contrasena, correo) => {
+    q = `SELECT contrasena FROM Usuario WHERE correo = ?;`;
+    const data = await pool.query(q,[correo]);
+    var band = false;
+    var hashedContasena = data[0][0].contrasena;
     await bcrypt.compare(contrasena, hashedContasena).then((result) => {
         band=result;
     });
@@ -39,13 +50,12 @@ APP.post('/api/post/empleado',
     [
         body('nombre').not().isEmpty().isAlpha(),
         body('usuario').not().isEmpty().isAlpha(),
-        body('contrasena').not().isEmail()
+        body('contrasena').not().isEmpty()
     ],
     async (req,res)=>{
         try{
             let body = req.body;
             let contrasena = await genContrasena(body.contrasena)
-            console.log(contrasena)
             q = `INSERT INTO Empleado VALUES (NULL,?,?,?);`
             const data = await pool.query(q, [body.nombre, body.usuario, contrasena]);
             res.send({success:true, message: 'Empleado creado con éxito'})
@@ -54,6 +64,129 @@ APP.post('/api/post/empleado',
         }
     }
 )
+
+APP.post('/api/post/login/empleado',
+    [
+        body('usuario').not().isEmpty().isAlpha(),
+        body('contrasena').not().isEmpty()
+    ],
+    async (req,res)=>{
+        try{
+            let body = req.body;
+            let comprobacion = await revContrasena(body.contrasena, body.usuario);
+            if(comprobacion){
+                q = `SELECT * FROM Empleado WHERE usuario = ?;`;
+                const data = await pool.query(q,[body.usuario]);
+                res.send({success:true, message: 'Inicio de sesión correcto', data: data[0][0]})
+            }else{
+                res.send({success:false, message: 'Usuario o contraseña incorrecta'})
+            }
+            
+        }catch(err){
+            res.send({success: false, message: err})
+        }
+    }
+)
+
+
+APP.post('/api/post/login/usuario',
+    [
+        body('correo').not().isEmpty().isAlpha(),
+        body('contrasena').not().isEmpty()
+    ],
+    async (req,res)=>{
+        try{
+            let body = req.body;
+            let comprobacion = await revContrasenaUsuario(body.contrasena, body.correo);
+            if(comprobacion){
+                q = `SELECT * FROM Usuario WHERE correo = ?;`;
+                const data = await pool.query(q,[body.correo]);
+                res.send({success:true, message: 'Inicio de sesión correcto', data: data[0][0]})
+            }else{
+                res.send({success:false, message: 'Correo o contraseña incorrecta'})
+            }
+            
+        }catch(err){
+            res.send({success: false, message: err})
+        }
+    }
+)
+
+
+APP.post('/api/post/prestamo',
+    [
+        body('usuario_empleado').not().isEmpty().isAlpha(),
+        body('contrasena').not().isEmpty(),
+        body('usuario_cliente').not().isEmpty(),
+        body('monto').not().isEmpty(),
+        body('meses').not().isEmpty().isNumeric(),
+        body('interes').not().isEmpty().isNumeric()
+    ],
+    async (req,res)=>{
+        const conexion = await pool.getConnection();
+       
+        try{
+            let body = req.body;
+            let comprobacion = await revContrasena(body.contrasena, body.usuario_empleado);
+            if(comprobacion){
+                
+                await conexion.beginTransaction()
+                q = `SELECT * FROM Empleado WHERE usuario = ?;`;
+                let empleado = await conexion.query(q,[body.usuario_empleado]);
+                q = `INSERT INTO Solicitud VALUES (NULL,?,?,?,?,?,?,?);`
+                let date = new Date();
+                let formatDate = date.toISOString()
+                formatDate = formatDate.split('T')[0]
+                let prestamo = await conexion.query(q, [body.monto, body.meses, body.interes, formatDate, null, false, empleado[0][0].idempleado]);
+                let idPrestamo = prestamo[0].insertId;
+                q = `INSERT INTO usuario_solicitud VALUES (?,?)`;
+                let solicitud = await conexion.query(q, [idPrestamo, body.usuario_cliente]);
+                await conexion.commit();
+                res.send({success:true, message: 'Solicitud creada con éxito'})
+            }else{
+                res.send({success:false, message: 'Usuario incorrecto, acceda al sistema para poder crear solicitudes'})
+            }
+        }catch(err){
+            conexion.release();
+            res.send({success: false, message: err})
+        }
+    }
+)
+
+APP.post('/api/post/arpovar/prestamo',
+    [
+        body('usuario_empleado').not().isEmpty().isAlpha(),
+        body('contrasena').not().isEmpty(),
+        body('id_solicitud').not().isEmpty()
+    ],
+    async (req,res)=>{
+        const conexion = await pool.getConnection();
+       
+        try{
+            let body = req.body;
+            let comprobacion = await revContrasena(body.contrasena, body.usuario_empleado);
+            if(comprobacion){
+                
+                await conexion.beginTransaction()
+                q = `SELECT * FROM Empleado WHERE usuario = ?;`;
+                let empleado = await conexion.query(q,[body.usuario_empleado]);
+                q = `UPDATE Solicitud SET aprovado=TRUE, fecha_aprovado=? WHERE idsolicitud = ?`
+                let date = new Date();
+                let formatDate = date.toISOString()
+                formatDate = formatDate.split('T')[0]
+                let prestamo = await conexion.query(q, [formatDate, body.id_solicitud]);
+                await conexion.commit();
+                res.send({success:true, message: 'Solicitud aprovada con éxito'})
+            }else{
+                res.send({success:false, message: 'Usuario incorrecto, acceda al sistema para poder crear solicitudes'})
+            }
+        }catch(err){
+            conexion.release();
+            res.send({success: false, message: err})
+        }
+    }
+)
+
 
 APP.post('/api/post/usuario',
     [
@@ -95,24 +228,53 @@ APP.post('/api/post/usuario',
 )
 
 
+
 APP.get('/api/get/estados',async (req,res)=>{
-    const data = await pool.query('SELECT * FROM Estado');
-    res.send(data[0])
+    try{
+        const data = await pool.query('SELECT * FROM Estado');
+        res.send({success:true, message: "Datos recuperados con éxito", data: data[0]})
+    }catch(err){
+        res.send({success:false, message: "Datos recuperados con éxito", data: err})
+    }
+    
 })
 
 APP.get('/api/get/municipio',async (req,res)=>{
-    const data = await pool.query('SELECT * FROM Municipio');
-    res.send(data[0])
+    try{
+        const data = await pool.query('SELECT * FROM Municipio');
+        res.send({success:true, message: "Datos recuperados con éxito", data: data[0]})
+    }catch(err){
+        res.send({success:false, message: "Datos recuperados con éxito", data: err})
+    }
 })
 
 APP.get('/api/get/pais',async (req,res)=>{
-    const data = await pool.query('SELECT * FROM Pais');
-    res.send(data[0])
+    try{
+        const data = await pool.query('SELECT * FROM Pais');
+        res.send({success:true, message: "Datos recuperados con éxito", data: data[0]})
+    }catch(err){
+        res.send({success:false, message: "Datos recuperados con éxito", data: err})
+    }
 })
 
-APP.get('/api/get/solicitud',async (req,res)=>{
-    const data = await pool.query('SELECT * FROM Solicitud');
-    res.send(data[0])
+APP.post('/api/post/solicitudes',
+    [
+        body('correo').not().isEmpty(),
+        body('contrasena').not().isEmpty()
+    ]
+    ,async (req,res)=>{
+    try{
+        let body = req.body;
+        let comprobacion = await revContrasenaUsuario(body.contrasena, body.correo);
+        if(comprobacion){
+            q = `SELECT * FROM Usuario WHERE correo = ?;`;
+            let usuario = await pool.query(q,[body.correo]);
+            const data = await pool.query('SELECT S.* FROM Solicitud S, usuario_solicitud US WHERE US.usuario_idusuario=? and S.idsolicitud=US.solicitud_idsolicitud',[usuario[0][0].idusuario]);
+            res.send({success:true, message: "Datos recuperados con éxito", data: data[0]})
+        }
+    }catch(err){
+        res.send({success:false, message: "Datos recuperados con éxito", data: err})
+    }
 })
 
 
--- Backend/sql/connection.js
+++ Backend/sql/connection.js
@@ -8,7 +8,24 @@ const pool = mysql.createPool({
     port: process.env['PORT_DB']||3306,
     waitForConnections: true,
     connectionLimit: 10, // Número máximo de conexiones en el pool
-    queueLimit: 0
+    queueLimit: 0,
+    "typeCast": function castField( field, useDefaultTypeCasting ) {
+
+        // We only want to cast bit fields that have a single-bit in them. If the field
+        // has more than one bit, then we cannot assume it is supposed to be a Boolean.
+        if ( ( field.type === "BIT" ) && ( field.length === 1 ) ) {
+
+            var bytes = field.buffer();
+
+            // A Buffer in Node represents a collection of 8-bit unsigned integers.
+            // Therefore, our single "bit field" comes back as the bits '0000 0001',
+            // which is equivalent to the number 1.
+            return( bytes[ 0 ] === 1 );
+
+        }
+
+        return( useDefaultTypeCasting() );
+    }
 });
 
 module.exports = pool.promise();
